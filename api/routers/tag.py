29a26bc360f72bd2089e5e7c6813bef7cdbbdf5c
Tofu Masume <tofu.masume@gmail.com>
2025-09-22 02:58:04 +0900

    style: formatting

--- api/routers/tag.py
+++ api/routers/tag.py
@@ -1,39 +1,33 @@
 from fastapi import APIRouter
 
-
 router = APIRouter()
 
 
 @router.get("/tags")
 async def get_tags():
-    """タグ一覧/基本情報取得
-    """
+    """タグ一覧/基本情報取得"""
     pass
 
 
 @router.post("/tags")
 async def create_tag():
-    """タグ作成
-    """
+    """タグ作成"""
     pass
 
 
 @router.get("/tags/{tag_id}")
 async def get_tag_detail():
-    """タグ詳細取得
-    """
+    """タグ詳細取得"""
     pass
 
 
 @router.put("/tags/{tag_id}")
 async def edit_tag():
-    """タグ編集
-    """
+    """タグ編集"""
     pass
 
 
 @router.delete("/tags/{tag_id}")
 async def delete_tag():
-    """タグ削除
-    """
+    """タグ削除"""
     pass
